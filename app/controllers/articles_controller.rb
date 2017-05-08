9cb26770208e15fdcd28817d0c9fc65252d4e9ce
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2017-05-08 14:55:10 -0300

    add authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :require_user, except: [:index, :show]
+
   def index
     @articles = Article.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  get 'signup' => 'users#new'
+  resources :users
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   root 'home#index'
 end
